404e113460f26c9fd198d847316571f19e367ac0
Toktobekov Kylymbek <kylymbekkemin@gmail.com>
2023-06-07 16:52:51 +0600

    n

--- server.mjs
+++ server.mjs
@@ -1,6 +1,17 @@
-import { createServer } from 'http';
+const jsonServer = require("json-server");
+const auth = require("json-server-auth");
+const app = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
 
-createServer((req, res) => {
-  res.write('Hello World!');
-  res.end();
-}).listen(process.env.PORT);
+// /!\ Bind the router db to the app
+app.db = router.db;
+
+// You must apply the auth middleware before the router
+
+app.use(auth);
+app.use(middlewares);
+app.use(router);
+app.listen(process.env.PORT, () => {
+  console.log("JSON Server is running");
+});
